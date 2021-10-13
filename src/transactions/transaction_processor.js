777417efaadfab6f2d314db80eca435d1ce2aa1b
Leonardo A <brinfo.leo@gmail.com>
2021-10-13 16:26:31 +0100

    applying reduce instead of for

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,21 +1,31 @@
-
-
-function processTransactions(transActions = null) {
-
+function processTransactions(transActions) {
+    txr = [];
+  
     if (!transActions) throw new Error("Undefined collection of transactions");
- 
+  
+    //const numberOfTransactions = transActions.length;
     let txCount=transActions.reduce((acc, item) => {
-        !acc[item] ? acc[item] = 1 : acc[item]++;
-        return acc;
-    },{});
-    return sortByAmountThenName(txCount);
-}
-
-const sortByAmountThenName = (txCount) => {
-    let sortedKeys = Object.keys(txCount).sort((itemOne, itemTwo) => {
-        return txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)
+      !acc[item] ? acc[item] = 1 : acc[item]++;
+      return acc;
+  },{});
+  
+  
+    txCount = sortByAmountThenName(txCount);
+  
+    // Place them back in array for returning
+    Object.keys(txCount).forEach(function (key, index) {
+      txr[index] = `${key} ${txCount[key]}`;
     });
-    return sortedKeys.map(item=>`${item} ${txCount[item]}`);
-}
-
-module.exports = processTransactions;
+  
+    return txr;
+  }
+  
+  function sortByAmountThenName(txCount) {
+   
+    return Object.entries(txCount)
+      .sort((cur,next) => next[1]>cur[1] && next[0]>cur[0]? 1:-1)
+      .reduce((arr, [key, val]) => ({ ...arr, [key]: val }), {})
+  
+  }
+  
+  module.exports = processTransactions;
